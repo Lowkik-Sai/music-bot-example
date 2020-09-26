b92d1156be07d9038d7ad675392302da77489d26
Lowkik-Sai <64127668+Lowkik-Sai@users.noreply.github.com>
2020-09-26 09:25:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -55,6 +55,18 @@ bot.on("message", async (message) => { // eslint-disable-line
         message.author.send(helpembed);
         message.channel.send(helpembed);
     }
+    if (command === "serverinfo" || command === "si") {
+        const helpembed = new MessageEmbed()
+            .setColor("RED")
+            .setTitle("Server Info")
+            .setImage(message.guild.iconURL)
+            .setDescription(`${message.guild}'s information`)
+            .addField("Owner", `The owner of this server is ${message.guild.owner}`)
+            .addField("Member Count", `This server has ${message.guild.memberCount} members`)
+            .addField("Emoji Count", `This server has ${message.guild.emojis.cache.size} emojis`)
+            .addField("Roles Count", `This server has ${message.guild.roles.cache.size} roles`)
+        message.channel.send(helpembed);
+    }
     if (command === "help" || command === "cmd") {
         const helpembed = new MessageEmbed()
             .setColor("BLUE")
