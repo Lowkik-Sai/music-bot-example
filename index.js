540b3aa54c0499d6b98f9fa2738f565a81f9e0d8
Lowkik-Sai <64127668+Lowkik-Sai@users.noreply.github.com>
2020-09-26 12:44:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -61,15 +61,6 @@ bot.on("message", async (message) => { // eslint-disable-line
     const m = await message.channel.send("Ping?");
     m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
     }
-    if(command === "say") {
-    // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
-    // To get the "message" itself we join the `args` back into a string with spaces: 
-    const sayMessage = args.join(" ");
-    // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
-    message.delete().catch(O_o=>{}); 
-    // And we get the bot to say the thing: 
-        message.channel.send(Message);
-    }
     if (command === "serverinfo" || command === "si") {
         const helpembed = new MessageEmbed()
             .setColor("RED")
