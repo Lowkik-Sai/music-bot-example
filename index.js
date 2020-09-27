3b324cf3210932eb643b2725685dd1a200c84c40
Lowkik-Sai <64127668+Lowkik-Sai@users.noreply.github.com>
2020-09-27 17:12:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -97,7 +97,7 @@ bot.on("message", async (message) => { // eslint-disable-line
   .on('error', console.error);
  
         await Embeds.build();
-        message.channel.send(helpembed);
+        message.channel.send(Embeds);
     }
     if (command === "serverinfo" || command === "si") {
         const helpembed = new MessageEmbed()
