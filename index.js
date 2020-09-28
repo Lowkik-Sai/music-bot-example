330d88a0f8702612edeb105352c24965bbb90aba
Lowkik-Sai <64127668+Lowkik-Sai@users.noreply.github.com>
2020-09-28 20:21:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -50,11 +50,9 @@ bot.on("message", async (message) => { // eslint-disable-line
             .setColor("BLUE")
             .setAuthor("Invite Link", message.author.displayAvatarURL())
             .setDescription(`[Click here!](https://discord.com/api/oauth2/authorize?client_id=758889056649216041&permissions=8&scope=bot)`)
-            .addField("Note:", `This message will be automatically deleted after 5min,Use this command again after expiry or check your Dm!`)
             .setTimestamp()
             .setFooter("Among Us Official", "https://cdn.discordapp.com/attachments/758709208543264778/758904787499745310/Screenshot_2020-09-25-09-45-28-68.jpg");
-        message.channel.send(helpembed).then(m => m.delete({
-                        timeout: 300000}))
+        message.channel.send(helpembed);
     }
     if (command === "invite" || command === "inv") {
         const helpembed = new MessageEmbed()
