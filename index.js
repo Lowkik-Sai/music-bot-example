ed74ae6d1509f6f09b442a8722f065d8781f7b98
Lowkik-Sai <64127668+Lowkik-Sai@users.noreply.github.com>
2020-09-30 10:41:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -86,6 +86,14 @@ bot.on("message", async (message) => { // eslint-disable-line
         message.channel.send(helpembed);
 
     }
+    if(command === 'corona') {
+    if (!args[0]) return message.reply('you need to enter a country name')
+    const url = `https://coronavirus-19-api.herokuapp.com/countries/${args[0]}`
+    req(url, function(err, response, body){
+        if(err) return msg.reply("err")
+        body = JSON.parse(body)
+        msg.reply(`Corona Fälle für: + \*\*${body.cases}\*\*\ ` ,)
+    }
     if (command === "say" ) { 
         let msg;
         let textChannel = message.mentions.channels.first()
