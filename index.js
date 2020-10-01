24d4ebcd02ff9880ba9a5b68942bbb6f7c6e3e66
Lowkik-Sai <64127668+Lowkik-Sai@users.noreply.github.com>
2020-10-01 08:12:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -81,6 +81,7 @@ bot.on("message", async (message) => { // eslint-disable-line
         }
     }
     if (command === "covid" ) { 
+        async run (client, message, args)
         const fetch = require('node-fetch');
 
         let countries = args.join(" ");
