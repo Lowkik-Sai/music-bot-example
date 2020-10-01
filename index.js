c955cb77bea684f8f9105b0e51d40e9c11a0c344
Lowkik-Sai <64127668+Lowkik-Sai@users.noreply.github.com>
2020-10-01 13:43:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -80,6 +80,38 @@ bot.on("message", async (message) => { // eslint-disable-line
             message.channel.send(msg)
         }
     }
+    if (command === "ban" ) {
+        if (!cache.get(req.message.author.id)) {
+      var execArray = this.regPattern.exec(req.command.suffixe)
+      var userId = execArray[1]
+      var reason = execArray[2] !== '' ? execArray[2].replace(/^\s+|\s+$/g, '') : 'No reason'
+      var target = req.message.channel.guild.members.get(userId)
+      if (target) {
+        if (target.roles.has(config.modRole)) return
+        if (target._roles.some(role => config.disabledRoleBan.indexOf(role) >= 0)) return
+        if (target.user.bot && !config.allowBotBan) return
+        if (!config.allowHigherRankBan) {
+          var roleIndexesReq = req.message.member.roles.map((role) => {
+            return role.calculatedPosition
+          })
+          roleIndexesReq.sort()
+          var roleIndexesTarget = target.roles.map((role) => {
+            return role.calculatedPosition
+          })
+          roleIndexesTarget.sort()
+          if (roleIndexesReq[roleIndexesReq.length - 1] < roleIndexesTarget[roleIndexesTarget.length - 1]) return
+        }
+      }
+      cache.set(req.message.author.id, true)
+      console.log(`Banning command request from id: ${req.message.author.id} - ${req.message.author.username}\n Ban id requested: ${userId} - ${target != null ? target.user.username : 'Unknown name'}\nReason: ${reason}`)
+      req.channel.guild.ban(userId, { reason: reason }).then(() => {
+        if (config.modLog) req.client.channelLog.send('', { embed: { description: `User banned by ${req.message.author.username} - ${req.message.author.id}\nReason: ${reason}`, title: `New ban: ${userId} - ${target != null ? target.user.username : 'Unknown name'}` } })
+        console.log(` Banned: ${userId} at ${new Date().toJSON().slice(0, 20).replace(/-/g, '/')}`)
+      }).catch((err) => {
+        console.log(`Can't ban\n ${err.message}`)
+        req.channel.send('Can\'t ban')
+      })
+    }
     if (command === "covid" ) { 
         const fetch = require('node-fetch');
 
