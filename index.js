926994f185b21e31e301dc78da5975bb03530808
Lowkik-Sai <64127668+Lowkik-Sai@users.noreply.github.com>
2020-10-02 20:48:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const queue = new Map();
 
 bot.on("ready", () => {
   // This event will run if the bot starts, and logs in, successfully.
-  console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
+  console.log(`Bot has started, with ${bot.users.size} users, in ${bot.channels.size} channels of ${bot.guilds.size} guilds.`); 
   // Example of changing the bot's playing game to something useful. `client.user` is what the
   // docs refer to as the "ClientUser".
 });
