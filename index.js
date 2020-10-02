93614d71745229eca1683b90b516692ca9d51cc8
Lowkik-Sai <64127668+Lowkik-Sai@users.noreply.github.com>
2020-10-02 19:39:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -210,22 +210,21 @@ bot.on("message", async (message) => { // eslint-disable-line
         message.channel.send(helpembed);
     }
     if (command === "userinfo" || command === "ui" || command === "whois" ) {
-       let member = message.mentions.users.first() || message.author
+       let user = message.mentions.users.first() || message.author;
+ 
+    let embed = new MessageEmbed()
+        .setAuthor(user.username + '#' + user.discriminator, user.displayAvatarURL)
+        .setDescription(`${user}`)
+        .setColor(`RANDOM`)
+        .setThumbnail(`${user.displayAvatarURL}`)
+        .addField('Status:', user.presence.status, true)
+        .addField('Roles:', user.roles.map(r => `${r}`).join(' | '), true)
+        .setFooter(`ID: ${user.id}`)
+        .setTimestamp();
 
- const helpembed = new MessageEmbed()
-    .setColor("RANDOM")
-    .setThumbnail(`${user.displayAvatarURL}`)
-    .addField(`${user.tag}`, `${user}`, true)
-    .addField("ID:", `${user.id}`, true)
-    .addField("Nickname:", `${member.nickname !== null ? `${member.nickname}` : 'None'}`, true)
-    .addField("Status:", `${user.presence.status}`, true)
-    .addField("In Server", message.guild.name, true)
-    .addField("Game:", `${user.presence.game ? user.presence.game.name : 'None'}`, true)
-    .addField("Bot:", `${user.bot}`, true) 
-    .addField("Roles:", member.roles.map(roles => `${roles}`).join(', '), true)
-    .setFooter(`Replying to ${message.author.username}#${message.author.discriminator}`)
-message.channel.send({helpembed});
-    }    
+    message.channel.send({ embed: embed });
+    return;
+    }
     if (command === "botinfo" || command === "bi") {
         const helpembed = new MessageEmbed()
             .setColor("ORANGE")
