a2752d459b7f73e32eec85377d83771a49a4dc96
Lowkik-Sai <64127668+Lowkik-Sai@users.noreply.github.com>
2020-10-07 08:24:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -114,7 +114,7 @@ bot.on("message", async (message) => { // eslint-disable-line
             message.channel.send(msg)
         }
     }
-    if (command === 'stats') {
+    if (command === 'stats' && msg.author.id === '654669770549100575') {
       let m = '';
       m += `I am aware of ${message.guild.channels.cache.size} channels\n`;
       m += `I am aware of ${message.guild.members.cache.size} members\n`;
