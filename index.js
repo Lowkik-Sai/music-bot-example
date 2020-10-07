b5566a1707d1fa8f73f5f983dc054b0bd65d8e3b
Lowkik-Sai <64127668+Lowkik-Sai@users.noreply.github.com>
2020-10-07 19:52:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -436,20 +436,21 @@ message.channel.send({embed});
             message.delete();
         }
     
-        // Member doesn't have permissions
-        if (!message.member.hasPermission("MANAGE_MESSAGES")) {
-            return message.reply("You can't delete messages....").then(m => m.delete(5000));
-        }
+        // Member doesn't have permission
+    if (!message.member.hasPermission("MANAGE_MESSAGES")) {
+        return message.channel.send("You can't delete messages...").then(m => m.delete(5000));
+    }
 
-        // Check if args[0] is a number
-        if (isNaN(args[0]) || parseInt(args[0]) <= 0) {
-            return message.reply("Yeah.... That's not a numer? I also can't delete 0 messages by the way.").then(m => m.delete(5000));
-        }
+    // Check if args[0] is a number
+    if (isNaN(args[0]) || parseInt(args[0]) <= 0) {
+        return message.channel.send("Yeah... That's not a number? I also can't delete 0 messages by the way.").then(m => m.delete(5000));
 
-        // Maybe the bot can't delete messages
-        if (!message.guild.me.hasPermission("MANAGE_MESSAGES")) {
-            return message.reply("Sorryy... I can't delete messages.").then(m => m.delete(5000));
-        }
+    }
+
+    // Maybe the bot can't delete messages
+    if (!message.guild.me.hasPermission("MANAGE_MESSAGES")) {
+        return message.channel.send("Sorry... I can't delete messages.").then(m => m.delete(5000));
+    }
 
         let deleteAmount;
 
