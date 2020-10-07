b6988a69098a1ecaaa79d5dd810ff21eb604bf02
Lowkik-Sai <64127668+Lowkik-Sai@users.noreply.github.com>
2020-10-07 10:11:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const youtube = new YouTube(process.env.YTAPI_KEY);
 const queue = new Map();
 
 setInterval(function(){
-let st=['I'm Ok Now!','+help','+invite','Dm me for help!','Among Us Official India'];
+let st=["I'm Ok Now!" ,"+help" ,"+invite" ,"Dm me for help!" ,"Among Us Official India"];
 let sts= st[Math.floor(Math.random()*st.length)];
 bot.user.setPresence({ activity: { name: sts }, status: 'online' })
 .catch(console.error);
