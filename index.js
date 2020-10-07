84523a9b54ba4b578a3f1d4169e6361fa7915ced
Lowkik-Sai <64127668+Lowkik-Sai@users.noreply.github.com>
2020-10-07 20:18:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -432,38 +432,37 @@ message.channel.send({embed});
     }
   }
     if (command === "purge" || command === "clear") {
-       if (message.deletable) {
-            message.delete();
-        }
-    
-        // Member doesn't have permission
-    if (!message.member.hasPermission("MANAGE_MESSAGES")) {
-        return message.channel.send("You can't delete messages...");
-    }
-
-    // Check if args[0] is a number
-    if (isNaN(args[0]) || parseInt(args[0]) <= 0) {
-        return message.channel.send("Yeah... That's not a number? I also can't delete 0 messages by the way.");
-
-    }
-
-    // Maybe the bot can't delete messages
-    if (!message.guild.me.hasPermission("MANAGE_MESSAGES")) {
-        return message.channel.send("Sorry... I can't delete messages.");
-    }
-
-        let deleteAmount;
-
-        if (parseInt(args[0]) > 100) {
-            deleteAmount = 100;
-        } else {
-            deleteAmount = parseInt(args[0]);
-        }
-
-        message.channel.bulkDelete(deleteAmount, true)
-            .then(deleted => message.channel.send(`I deleted \`${deleted.size}\` messages.`))
-            .catch(err => message.reply(`Something went wrong... ${err}`));
-    }
+         // Parse args
+  let deleteCount = parseInt(args[0], 10)
+  // Check if Delete Count is bigger than discord can handle
+  if (deleteCount > 100) {
+    // Delete only 100 messages
+    deleteCount = 100
+    // Notify user about this
+    message.channel.send('Can handle only 100 messages. Deleting 100 messages').then(e => setTimeout(() => e.delete(), 2000))
+  }
+  // Delete only one message if not specified how many
+  if (Number.isNaN(deleteCount)) deleteCount = 1
+  // console.log([deleteCount, typeof (deleteCount)])
+  try {
+    // Delete user message
+    await message.delete()
+    // Fetch messages
+    const fetched = await message.channel.messages.fetch({ limit: deleteCount })
+    // Delete messages
+    await message.channel.bulkDelete(fetched)
+  } catch (error) {
+    // Handle error
+    message.reply(`Couldn't delete messages because of: ${ error }`)
+    // Delete error message
+      .then(el => setTimeout(() => el.delete(), 5000))
+    // Exit function
+    return 0
+  }
+  // Notify if success
+  message.channel.send(`Successfully deleted ${ deleteCount } ${ deleteCount === 1 ? 'message' : 'messages' }.`).then(el => setTimeout(() => el.delete(), 3000))
+  return 0
+}
     if (command === "play" || command === "p") {
         const voiceChannel = message.member.voice.channel; 
         if (!voiceChannel) return message.channel.send({
