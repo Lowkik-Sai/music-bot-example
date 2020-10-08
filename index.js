e1f660e7776d2a5db52dbe393d9b5f3d9da9ac7e
Lowkik-Sai <64127668+Lowkik-Sai@users.noreply.github.com>
2020-10-08 15:22:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -220,6 +220,43 @@ bot.on("message", async (message) => { // eslint-disable-line
 
         }
     }
+    if (command === "uptime" ) {
+       let days = 0;
+       let week = 0;
+       let uptime = ``;
+       let totalSeconds = (client.uptime / 1000);
+       let hours = Math.floor(totalSeconds / 3600);
+       totalSeconds %= 3600;
+       let minutes = Math.floor(totalSeconds / 60);
+       let seconds = Math.floor(totalSeconds % 60);
+
+       if(hours > 23){
+           days = days + 1;
+           hours = 0;
+       }
+
+       if(days == 7){
+           days = 0;
+           week = week + 1;
+       }
+
+       if(week > 0){
+           uptime += `${week} week, `;
+       }
+
+       if(minutes > 60){
+           minutes = 0;
+       }
+
+       uptime += `${days} days, ${hours} hours, ${minutes} minutes and ${seconds} seconds`;
+
+       let serverembed = new MessageEmbed()
+           .setColor("#228B22")
+           .addField('Uptime', uptime);
+
+       message.channel.send(serverembed);
+
+}
     if (command === "leave"&&message.author.id=="654669770549100575") {
        message.channel.send({embed: {
   color: 3066993,
