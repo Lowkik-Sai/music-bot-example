b53801cf752ae29f6f1ad5675a820eea89c03ad0
Lowkik-Sai <64127668+Lowkik-Sai@users.noreply.github.com>
2020-10-08 09:02:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -208,8 +208,8 @@ bot.on("message", async (message) => { // eslint-disable-line
     }
     if (command === "leave"&&message.author.id=="654669770549100575") {
       message.guild
-        .then(guild => message.channel.send('Bye,Bye...'))
         .leave()
+        .then(guild => message.channel.send('Bye,Bye...'))
         .then(guild => console.log('Left guild', guild.name))
         .catch(console.error);
     }
