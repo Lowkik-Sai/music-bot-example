c261981bdc51b9fc4786410c6d0811b04c680197
Lowkik-Sai <64127668+Lowkik-Sai@users.noreply.github.com>
2020-10-08 13:35:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -220,6 +220,12 @@ bot.on("message", async (message) => { // eslint-disable-line
 
         }
     }
+    if (command === "leave"message.author.id=="654669770549100575") {
+       message.channel.send{embed: {
+  color: 3066993,
+  description:Bye Bye...
+}})
+    }
     if (command === "leave"&&message.author.id=="654669770549100575") {
       message.guild
         .leave()
