173cdb14bfaeaf0dcdd3c75031f6bd440348c01b
Lowkik-Sai <64127668+Lowkik-Sai@users.noreply.github.com>
2020-10-09 14:43:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -466,41 +466,41 @@ bot.on("message", async (message) => { // eslint-disable-line
     }
 }
     if (command === "purge" || command === "clear" ) {
-       const hasPerms = require("./helpers.js").hasPerms;
-       const inPrivate = require("./helpers.js").inPrivate;
+        const Discord = require('discord.js')
+        console.log("purging messages")
 
-    if (inPrivate(msg)) {
-      msg.channel.send("You Cant Purge Message In DM's!");
-      return;
-    }
-    if (hasPerms(msg)) {
-      let newamount = 0;
-      if (!suffix) {
-        newamount = 2;
-      } else {
-        let amount = Number(suffix);
-        let adding = 1;
-        newamount = amount + adding;
-      }
-      let messagecount = newamount.toString();
-      msg.channel
-        .fetchMessages({
-          limit: messagecount
-        })
-        .then(messages => {
-          msg.channel.bulkDelete(messages);
-          // Logging the number of messages deleted on both the channel and console.
-          msg.channel.send('Deletion of messages successful. \n Total messages deleted including command: ' + newamount).then(message => message.delete(5000));
-          console.log('Deletion of messages successful. \n Total messages deleted including command: ' + newamount);
-        })
-        .catch(err => {
-          console.log('Error while doing Bulk Delete');
-          console.log(err); 
+        const amount = parseInt(args[0]) + 1;
+
+        if (isNaN(amount)) {
+            return message.reply('that doesn\'t seem to be a valid number.');
+        } else if (amount <= 1 || amount > 100) {
+            return message.reply('you need to input a number between 1 and 99.');
+        }
+
+        message.channel.bulkDelete(amount, true).then(deletedMessages => {
+            // Filter the deleted messages with .filter()
+            var botMessages = deletedMessages.filter(m => m.author.bot);
+            var userPins = deletedMessages.filter(m => m.pinned);
+            var userMessages = deletedMessages.filter(m => !m.author.bot);
+
+            const embed = new Discord.RichEmbed()
+                .setTitle("Success")
+                .setColor(0x00AE86)
+                .setFooter("Guardian", "https://raw.githubusercontent.com/phantomdev-github/Resources/master/Discord%20Bots/Guardian/src/avatar.png")
+                .setThumbnail("https://raw.githubusercontent.com/phantomdev-github/Resources/master/Discord%20Bots/Guardian/src/avatar.png")
+                .setTimestamp()
+                .setURL("https://github.com/phantomdev-github/Resources/tree/master/Discord%20Bots/Guardian")
+                .addField("Bot Messages Purged", botMessages.size, false)
+                .addField("User Pins Purged", userPins.size, false)
+                .addField("User Messages Purged", userMessages.size, false)
+                .addField("Total Messages Purged", deletedMessages.size, false);
+
+            message.channel.send(embed);
+        }).catch(err => {
+            console.error(err);
+            message.channel.send('there was an error trying to prune messages in this channel!');
         });
-    } else {
-      msg.channel.send('only moderators can use this command!').then(message => message.delete(5000));
     }
-  }
     if (command === "caronavirus" || command === "cv") {
               message.reply({embed: {
   color: 3447003,
