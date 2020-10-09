c36b1dad6ecc70277e1aa210246f710d0244f725
Lowkik-Sai <64127668+Lowkik-Sai@users.noreply.github.com>
2020-10-09 11:58:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -103,7 +103,7 @@ bot.on("message", async (message) => { // eslint-disable-line
         const helpembed = new MessageEmbed()
             .setColor("BLUE")
             .setAuthor("Invite Link", message.author.displayAvatarURL())
-            .setDescription(`[Click here!](https://discord.com/oauth2/authorize?client_id=758889056649216041&scope=bot&permissions=2146942463&response_type=code)`)
+            .setDescription(`[Click here!](https://discord.com/api/oauth2/authorize?client_id=758889056649216041&permissions=8&scope=bot)`)
             .setTimestamp()
             .setFooter("Among Us Official", "https://cdn.discordapp.com/attachments/758709208543264778/758904787499745310/Screenshot_2020-09-25-09-45-28-68.jpg");
         message.reply(helpembed);
@@ -704,7 +704,7 @@ const member = message.guild.member(user);
         .addField("My Prefix", `+`)
         .addField(`Servers`,`${servers}`, true)
         .addField(`Users`, `${users}`, true)
-        .addField(`Invite Me!`, `[Click Here!](https://discord.com/oauth2/authorize?client_id=758889056649216041&scope=bot&permissions=2146942463&response_type=code)`, true)
+        .addField(`Invite Me!`, `[Click Here!](https://discord.com/api/oauth2/authorize?client_id=758889056649216041&permissions=8&scope=bot)`, true)
         .setFooter(`Uptime: ${uptime}`);
 
     message.channel.send(serverembed);    
