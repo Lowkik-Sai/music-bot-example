66ddd8037068ce212bfddafcdbddbf72a4793758
Lowkik-Sai <64127668+Lowkik-Sai@users.noreply.github.com>
2020-10-10 08:19:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -227,7 +227,55 @@ bot.on("message", async (message) => { // eslint-disable-line
 
         message.channel.send('Success!')
     }
-}
+    if (command === "warn" ) {
+        const Discord = require('discord.js');
+        const db = require('quick.db');
+
+        if(!message.member.hasPermission("MANAGE_SERVER")) return message.channel.send('You can\'t use that');
+
+        const user = message.mentions.users.first() || message.guild.members.cache.get(args[0]);
+
+        if(!user) return message.channel.send('Please specify a user, via mention or ID');
+
+        if(user.bot) return message.channel.send('You can\'t warn bots');
+
+        if(message.author.id === user.id) return message.channel.send('You can\'t warn yourself nitwit');
+
+        if(message.guild.owner.id === user.id) return message.channel.send('You can\'t warn the server\'s owner');
+
+        let reason = args.slice(1).join(" ");
+
+        if(!reason) reason = 'Unspecified';
+
+        let warnings = db.get(`warnings_${message.guild.id}_${user.id}`);
+
+        if(warnings === 3) return message.channel.send(`${user} has already reached three warnings`);
+
+
+        if(warnings === null) {
+            db.set(`warnings_${message.guild.id}_${user.id}`, 1);
+            user.send(`You were warned in ${message.guild.name} for the follwoing reason: \`${reason}\``)
+            await message.channel.send(`**${user.username}** has been warned`)
+        }
+
+        if(warnings !== null){
+            db.add(`warnings_${message.guild.id}_${user.id}`, 1)
+            user.send(`You were warned in ${message.guild.name} for the follwoing reason: \`${reason}\``)
+            await message.channel.send(`**${user.username}** has been warned`)
+        }
+    }
+    if (command === "warnings" ) { 
+        const db = require('quick.db');
+
+        const user = message.mentions.users.first() || message.guild.members.cache.get(args[0]) || message.author;
+
+
+        let warnings = await db.get(`warnings_${message.guild.id}_${user.id}`);
+
+        if(warnings === null) warnings = 0;
+
+        message.channel.send(`**${user.username}** has *${warnings}* warning(s)`);
+    }
     if (command === "bal" ) {
         const db = require('quick.db');
         const Discord = require('discord.js');
