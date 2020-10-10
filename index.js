f5b28f3d95937c5de75bd105dbd148538c60851e
Lowkik-Sai <64127668+Lowkik-Sai@users.noreply.github.com>
2020-10-10 20:48:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -236,7 +236,7 @@ bot.on("message", async (message) => { // eslint-disable-line
     let searchTerm = args[0];
     if(!searchTerm) return message.channel.send("Please type a term to search!");
 
-    let matches = users.filter(u => u.tag.toLowerCase().includes(searchTerm.toLowerCase()));
+    let matches = users.cache.filter(u => u.tag.toLowerCase().includes(searchTerm.toLowerCase()));
     
     message.channel.send(matches.map(u => u.tag));
 
