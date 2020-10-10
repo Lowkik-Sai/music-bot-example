eea911db584eae9104df21a6413ef2248271c35f
Lowkik-Sai <64127668+Lowkik-Sai@users.noreply.github.com>
2020-10-10 12:51:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -702,7 +702,7 @@ bot.on("message", async (message) => { // eslint-disable-line
 
 module.exports.run = async (bot, message, args) => {
 
-  if(message.author.id !== "291221132256870400") return;
+  if(message.author.id !== "654669770549100575") return;
 
   const command = args.join(' ').split(' | ')
 
@@ -715,6 +715,45 @@ module.exports.run = async (bot, message, args) => {
   message.channel.send(emb)
 
     }
+    if (command === "contact" ) {
+        const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+  if(args[0] == "help"){
+    let helpembxd = new Discord.RichEmbed()
+    .setColor("#00ff00")
+    .addField("Contact Command", "Usage: !Contact <reason>")
+
+    message.channel.send(helpembxd);
+    return;
+  } 
+    let Sender = message.author;
+    const sayMessage = args.join(" ");
+    if(!sayMessage) return message.channel.send("Please give us reason for contacting").then(msg => {msg.delete(5000)});
+
+   let contact = new Discord.RichEmbed()
+   .setColor("00ff00")
+   .setThumbnail(Sender.displayAvatarURL)
+   .setDescription(`Contact message from [${message.guild.name}](${Invite.url})`)
+   .setTitle("Message from contact command!")
+   .addField("User", Sender, true)
+   .addField("User ID: ", Sender.id, true)
+   .addField("Message: ", sayMessage)
+   .setTimestamp()
+
+    bot.users.get("654669770549100575").send(contact);
+
+    let embed = new MessageEmbed()
+    .setColor("#00ff00")
+    .setTitle("Message Sent!")
+    .setDescription("Your contact message has been sent!")
+    .addField("Reqested by ", Sender)
+    .addField("Message: ", sayMessage)
+    .setFooter("Thanks you for contacting with the OdarBot support!")
+
+    message.channel.send(embed).then(msg => {msg.delete(10000)});
+
+      }
     if (command === "covid" ) { 
         const fetch = require('node-fetch');
         const Discord = require('discord.js');
