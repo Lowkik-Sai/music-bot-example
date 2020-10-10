90e4a79cbf0114cc8178f9750ffff47e843f9183
Lowkik-Sai <64127668+Lowkik-Sai@users.noreply.github.com>
2020-10-10 08:05:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -207,6 +207,27 @@ bot.on("message", async (message) => { // eslint-disable-line
             message.channel.send('```' + data + '```')
         })
     }
+    if (command === "deletewarns" || command === "delwarns" ) {
+        const db = require('quick.db');
+        const warnings = require('./warnings');
+        if(!message.member.hasPermission("MANAGE_SERVER")) return message.channel.send('You can\'t use that.');
+
+        const user = message.mentions.users.first() || message.guild.members.cache.get(args[0]);
+
+        if(!user) return message.channel.send('Please specify a user, via mention or ID');
+
+        if(user.bot) return message.channel.send('You can\'t warn bots');
+
+        if(user.id === message.author.id) return message.channel.send('You can\'t clear your own warnings');
+
+        if(warnings === null) return message.channel.send(`**${user.username} has no warnings**`);
+
+
+        db.delete(`warnings_${message.guild.id}_${user.id}`);
+
+        message.channel.send('Success!')
+    }
+}
     if (command === "bal" ) {
         const db = require('quick.db');
         const Discord = require('discord.js');
