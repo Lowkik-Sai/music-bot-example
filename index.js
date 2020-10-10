6f1d37ffc99059ee7be821a10747ca8ccce28163
Lowkik-Sai <64127668+Lowkik-Sai@users.noreply.github.com>
2020-10-10 10:36:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -383,6 +383,27 @@ bot.on("message", async (message) => { // eslint-disable-line
 }})
         }
     }
+    if (command === "work" ) {
+       const db = require('quick.db');
+       const ms = require('parse-ms');
+        let user = message.author;
+        let timeout = 600000;
+        let author = await db.fetch(`worked_${message.guild.id}_${user.id}`);
+
+        if(author !== null && timeout - (Date.now() - author) > 0){
+            let time = ms(timeout - (Date.now() - author));
+            return message.channel.send(`You cannot work again for ${time.minutes}m and ${time.seconds}s`)
+        } else {
+            let amount = Math.floor(Math.random() * 80) + 1;
+            db.add(`money_${message.guild.id}_${user.id}`, amount)
+            db.set(`worked_${message.guild.id}_${user.id}`, Date.now())
+
+            message.channel.send({embed: {
+    color: 3066993,
+    description:`${user}, you worked and earned ${amount} coins`
+}})
+        }
+    }
     if (command === "buy" ) {
        const db = require('quick.db');
        const Discord = require('discord.js');
