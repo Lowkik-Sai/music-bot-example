6314482de4a9f12908eb8ef56f2a577b75db8749
Lowkik-Sai <64127668+Lowkik-Sai@users.noreply.github.com>
2020-10-10 08:00:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -221,6 +221,27 @@ bot.on("message", async (message) => { // eslint-disable-line
   description:`${user} currently has ${bal} coins!`
 }});
     }
+    if (command === "daily" ) {
+       const db = require('quick.db');
+       const ms = require('parse-ms');
+
+        let user = message.author;
+        let timeout = 86400000;
+        let amount = 500;
+
+        let daily = await db.fetch(`daily_${message.guild.id}_${user.id}`);
+
+        if(daily !== null && timeout - (Date.now() - daily) > 0){
+            let time = ms(timeout - (Date.now() - daily));
+
+            return message.channel.send(`You've already collected your daily award. Come back in ${time.days}d, ${time.hours}h, ${time.minutes}m, and ${time.seconds}s`)
+        } else {
+            db.add(`money_${message.guild.id}_${user.id}`, amount);
+            db.set(`daily_${message.guild.id}_${user.id}`, Date.now());
+
+            message.channel.send(`Successfully added ${amount} coins to your account`)
+        }
+    }
     if (command === "buy" ) {
        const db = require('quick.db');
        const Discord = require('discord.js');
