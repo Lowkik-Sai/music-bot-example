cb75cb7be634494d99c7a0d165b246606240907d
Lowkik-Sai <64127668+Lowkik-Sai@users.noreply.github.com>
2020-10-10 12:13:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -514,6 +514,47 @@ bot.on("message", async (message) => { // eslint-disable-line
 
         message.channel.send(changeMyMind);
     }
+    if (command === "beautify" ) {
+    let res;
+    try {
+      await msg.channel.send("Searching for code to beautify...");
+      res = await format(msg);
+    } catch(e) {
+      res = e;
+    }
+    return msg.channel.send(res);
+  };
+  
+  const { js_beautify } = require("js-beautify");
+  
+  const reduceIndentation = (string) => {
+    let whitespace = string.match(/^(\s+)/);
+    if (!whitespace) return string;
+  
+    whitespace = whitespace[0].replace("\n", "");
+    return string.split("\n").map(line => line.replace(whitespace, "")).join("\n");
+  };
+  
+  const format = async (msg) => {
+    const messages = msg.channel.messages.array().reverse();
+    let code;
+    const codeRegex = /```(?:js|json|javascript)?\n?((?:\n|.)+?)\n?```/ig;
+  
+    for (let m = 0; m < messages.length; m++) {
+      const message = messages[m];
+      const groups = codeRegex.exec(message.content);
+  
+      if (groups && groups[1].length) {
+        code = groups[1];
+        break;
+      }
+    }
+    if (!code) throw new Error("No Javascript codeblock found.");
+  
+    const beautifiedCode = js_beautify(code, { indent_size: 2, brace_style: "collapse", jslint_happy: true });
+    const str = await reduceIndentation(beautifiedCode);
+    return (`${"```js"}\n${str}\n${"```"}`);
+  }
     if (command === "calculate" ) {
         const math = require('mathjs');
 
