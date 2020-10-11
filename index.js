ef794eaee08e6537f7ff9c0aeb40af4dcb026b47
Lowkik-Sai <64127668+Lowkik-Sai@users.noreply.github.com>
2020-10-11 07:36:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1517,7 +1517,7 @@ const member = message.guild.member(user);
         color: "#000000",
         permissions:[]
       })
-      message.guild.channels.cache.forEach((channel)=>{
+      message.guild.channels.cache.forEach(async (channel, id) => {
         await channel.overwritePermissions(muterole, {
           SEND_MESSAGES: false,
           ADD_REACTIONS: false
