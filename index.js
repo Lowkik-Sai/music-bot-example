5e2c74a954fb7971fc6623886d5542f7a94c6c42
Lowkik-Sai <64127668+Lowkik-Sai@users.noreply.github.com>
2020-10-12 22:41:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1782,6 +1782,7 @@ const member = message.guild.member(user);
     message.channel.send(serverembed);    
 
 }
+    if (command === "mute" ) {
     if (!message.member.hasPermission("MANAGE_ROLES")) {
       return message.channel.send(
         "Sorry but you do not have permission to mute anyone"
@@ -1836,6 +1837,40 @@ await message.channel.send(`You muted **${message.mentions.users.first().usernam
     
 //WE ARE DONE HERE 
     
+  }
+    if (command === "unmute" ) {
+    if (!message.member.hasPermission("MANAGE_ROLES")) {
+      return message.channel.send(
+        "Sorry but you do not have permission to unmute anyone"
+      );
+    }
+
+    if (!message.guild.me.hasPermission("MANAGE_ROLES")) {
+      return message.channel.send("I do not have permission to manage roles.");
+    }
+
+    const user = message.mentions.members.first();
+
+    if (!user) {
+      return message.channel.send(
+        "Please mention the member to who you want to unmute"
+      );
+    }
+    
+    let muterole = message.guild.roles.cache.find(x => x.name === "Muted")
+    
+    
+ if(user.roles.cache.has(muterole)) {
+      return message.channel.send("Given User do not have mute role so what i am suppose to take")
+    }
+    
+    
+    user.roles.remove(muterole)
+    
+    await message.channel.send(`**${message.mentions.users.first().username}** is unmuted`)
+    
+    user.send(`You are now unmuted from **${message.guild.name}**`)
+
   }
     if(command === "kick") {
     if (!message.guild) return;
