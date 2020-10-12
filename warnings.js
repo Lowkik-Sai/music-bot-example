f1e16b71d9e95686e4d74dffb8621732ef57e1f8
Lowkik-Sai <64127668+Lowkik-Sai@users.noreply.github.com>
2020-10-12 10:08:38 +0530

    Update warnings.js

--- warnings.js
+++ warnings.js
@@ -5,13 +5,24 @@ module.exports = {
     description: "Check a users warnings",
 
     async run (client, message, args){
-        const user = message.mentions.users.first() || message.guild.members.cache.get(args[0]) || message.author;
+        const id = args[0];
+        const user = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.author;
 
 
         let warnings = await db.get(`warnings_${message.guild.id}_${user.id}`);
 
         if(warnings === null) warnings = 0;
 
-        message.channel.send(`**${user.username}** has *${warnings}* warning(s)`);
+        message.channel.send({embed: {
+   color: 3066993,
+   description:`**${user.username}** has *${warnings}* warning(s)`
+}});
+
+        if(warnings !== null) warnings = 0;
+
+        message.channel.send({embed: {
+   color: 3066993,
+   description:`**<@${id}>** has *${warnings}* warning(s)`
+}});
     }
 }
