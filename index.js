24d78bdcdfac725aad49e67b61d2fd0ff5102076
Lowkik-Sai <64127668+Lowkik-Sai@users.noreply.github.com>
2020-10-13 08:31:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1099,6 +1099,38 @@ const OFFSET = '!'.charCodeAt(0);
     await message.channel.send(resultxD);
 
 }
+    if (command === "level" || command === "lvl" ) {
+        const db = require('quick.db')
+        const discord = require('discord.js')
+        const { getInfo } = require("./xp.js")
+        const user = message.mentions.users.first() || message.author;
+    
+    if(user.id === client.user.id) { //IF BOT
+      return message.channel.send("😉 | I am on level 100")
+    }
+    
+    if(user.bot) {
+      return message.channel.send("Bot do not have levels")
+    }
+    
+    let xp = db.get(`xp_${user.id}_${message.guild.id}`) || 0;
+    
+    const {level, remxp, levelxp} = getInfo(xp);
+    if(xp === 0) return message.channel.send(`**${user.tag}** is out of the xp`)
+    
+    let embed = new MessageEmbed()
+    .setAuthor(user.username, message.guild.iconURL())
+    .setColor("#ff2050")
+    .setThumbnail(user.avatarURL())
+    .setDescription(`**LEVEL** - ${level}
+**XP** - ${remxp}/${levelxp}`)
+    
+ message.channel.send(embed)   
+    
+    
+    
+    
+  }
     if (command === "imdb" ) {
         const discord = require("discord.js");
         const imdb = require("imdb-api");
