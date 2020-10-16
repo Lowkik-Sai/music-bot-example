a2c2a1195e1815c28a57c690ac51d1c0e8f20e91
Lowkik-Sai <64127668+Lowkik-Sai@users.noreply.github.com>
2020-10-16 10:59:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -401,19 +401,31 @@ bot.on("message", async (message) => { // eslint-disable-line
 
     }
     if (command === "slowmode" ) {
+const sayMessage = args.join(" ")
+    if(!sayMessage) return message.reply({embed: {
+  color: 3066993,
+  description:"Proper Usage : +slowmode <number in seconds> <reason>"
+}})
+
 if (!args[0])
-      return message.channel.send(
-        `You did not specify the time in seconds you wish to set this channel's slow mode too!`
-      );
-    if (isNaN(args[0])) return message.channel.send(`That is not a number!`);
+      return message.channel.send({embed: {
+  color: 3066993,
+  description:`You did not specify the time in seconds you wish to set this channel's slow mode too!`
+      }});
+    if (isNaN(args[0])) return message.channel.send({embed: {
+   color: 3066993,
+   description:`That is not a number!`
+}});
     let reason = args[1];
     if (!reason) {
       reason == "No reason provided!";
     }
     message.channel.setRateLimitPerUser(args[0], reason);
-    message.channel.send(
-      `Set the slowmode of this channel too **${args[0]}** with the reason: **${reason}**`
-    );
+    message.channel.send({embed: {
+    color: 3066993,
+    description:`Set the slowmode of this channel too **${args[0]}** with the reason: **${reason}**`,
+    footer: "To Stop Slowmode Type +slowmode 0 finished"
+}});
   }
     if (command === "timer" ) {
 
