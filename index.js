2588dd4149cf17d3a9b4c67dd46ce0d24cd85364
Lowkik-Sai <64127668+Lowkik-Sai@users.noreply.github.com>
2020-10-16 18:30:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -110,40 +110,39 @@ bot.on("voiceStateUpdate", (mold, mnew) => {
 	} ;
 });
 
+
 bot.on('guildMemberAdd', async member => {
-	
-	let wChan = db.fetch(`${member.guild.id}`)
-	
+    
+    let wChan = db.fetch(`${member.guild.id}`)
+
 	if(wChan == null) return;
-	
+
 	if(!wChan) return;
-	
-let font = await jimp.loadFont(jimp.FONT_SANS_32_BLACK) //We declare a 32px font
-  let font64 = await jimp.loadFont(jimp.FONT_SANS_64_WHITE) //We declare a 64px font
+    
+let font = await jimp.loadFont(jimp.FONT_SANS_64_BLACK) //We declare a 32px font
+  let font64 = await jimp.loadFont(jimp.FONT_SANS_64_WHITE)//We declare a 64px font
+  let font64b = await jimp.loadFont(jimp.FONT_SANS_64_WHITE)
   let bfont64 = await jimp.loadFont(jimp.FONT_SANS_64_BLACK)
   let mask = await jimp.read('https://i.imgur.com/552kzaW.png') //We load a mask for the avatar, so we can make it a circle instead of a shape
   let welcome = await jimp.read('http://rovettidesign.com/wp-content/uploads/2011/07/clouds2.jpg') //We load the base image
-
   jimp.read(member.user.displayAvatarURL).then(avatar => { //We take the user's avatar
     avatar.resize(200, 200) //Resize it
     mask.resize(200, 200) //Resize the mask
     avatar.mask(mask) //Make the avatar circle
     welcome.resize(1000, 300)
-	
-  welcome.print(font64, 265, 55, `Welcome ${member.user.username}`) //We print the new user's name with the 64px font
-  welcome.print(bfont64, 265, 125, `To ${member.guild.name}`)
-  welcome.print(font64, 265, 195, `There are now ${member.guild.memberCount} users`)
-  welcome.composite(avatar, 40, 55).write('Welcome2.png') //Put the avatar on the image and create the Welcome2.png bot
-  try{
-  member.guild.channels.cache.get(wChan).send(``, { files: ["Welcome2.png"] }) //Send the image to the channel
+    
+  welcome.print(font64, 265, 35, `Bienvenido ${member.user.username} a`) //We print the new user's name with the 64px font
+  welcome.print(bfont64, 265, 105, ` ${member.guild.name}`)
+  welcome.print(font64, 265, 175, `Gracias a ti ya somos `)
+  welcome.print(font64b, 265, 230, `${member.guild.memberCount} usuarios`)
+  welcome.composite(avatar, 40, 45).write('Welcome2.png') //Put the avatar on the image and create the Welcome2.png bot
+    try{
+  member.guild.channels.get(wChan).send(``, { files: ["Welcome2.png"] }) //Send the image to the channel
   }catch(e){
-	  // dont do anything if error occurs
-	  // if this occurs bot probably can't send images or messages
+      // dont do anything if error occurs
+      // if this occurs bot probably can't send images or messages
   }
   })
-
-	
-	
 });
 
 bot.on("message", async (message) => { // eslint-disable-line
