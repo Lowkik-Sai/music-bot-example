eed3deec118cd6a6b6182a1900eaadb97ec73f02
Lowkik-Sai <64127668+Lowkik-Sai@users.noreply.github.com>
2020-10-16 09:33:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2687,6 +2687,23 @@ if (!message.member.permissions.has("ADMINISTRATOR"))
             message.channel.send(message.author.toString()+ `Timer fini, il à durer: ${ms(ms(Timer), {long: true})}`)
         }, ms(Timer));
     }
+    if (command === "slowmode" ) {
+if (!args[0])
+      return message.channel.send(
+        `You did not specify the time in seconds you wish to set this channel's slow mode too!`
+      );
+    if (isNaN(args[0])) return message.channel.send(`That is not a number!`);
+    let reason = message.content.slice(
+      bot.prefix.length + 9 + args[0].length + 1
+    );
+    if (!reason) {
+      reason == "No reason provided!";
+    }
+    message.channel.setRateLimitPerUser(args[0], reason);
+    message.channel.send(
+      `Set the slowmode of this channel too **${args[0]}** with the reason: **${reason}**`
+    );
+  }
     if (command === "timer" ) {
 const { MessageEmbed } = require("discord.js");
 const { Timers } = require("./variable.js");
