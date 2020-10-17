906c6d6d99fcdbc90ab2a8415574d0ed5778dd98
Lowkik-Sai <64127668+Lowkik-Sai@users.noreply.github.com>
2020-10-17 10:54:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3026,7 +3026,176 @@ const member = message.guild.member(user);
     user.send(`You are now unmuted from **${message.guild.name}**`)
 
   }
-    if (command === "
+    if (command === "modeveryone" ) {
+        const botPerms = ['MANAGE_MESSAGES', 'KICK_MEMBERS', 'MANAGE_ROLES', 'MANAGE_CHANNELS'];
+
+	if (!message.guild.me.permissions.has(botPerms)) {
+		return message.reply(`I need the permissions ${botPerms.join(', ')} for this demonstration to work properly`);
+	}
+        const everyonePerms = new Permissions(message.guild.defaultRole.permissions);
+		const newPerms = everyonePerms.add(['MANAGE_MESSAGES', 'KICK_MEMBERS']);
+
+		message.guild.defaultRole.setPermissions(newPerms.bitfield)
+			.then(() => message.channel.send('Added mod permissions to `@everyone`.'))
+			.catch(console.error);
+    }
+    if(command === "unmodeveryone" ) {
+       const botPerms = ['MANAGE_MESSAGES', 'KICK_MEMBERS', 'MANAGE_ROLES', 'MANAGE_CHANNELS'];
+
+	if (!message.guild.me.permissions.has(botPerms)) {
+		return message.reply(`I need the permissions ${botPerms.join(', ')} for this demonstration to work properly`);
+	}
+        const everyonePerms = new Permissions(message.guild.defaultRole.permissions);
+		const newPerms = everyonePerms.remove(['MANAGE_MESSAGES', 'KICK_MEMBERS']);
+
+		message.guild.defaultRole.setPermissions(newPerms.bitfield)
+			.then(() => message.channel.send('Removed mod permissions from `@everyone`.'))
+			.catch(console.error);
+	}
+    if (command === "createmod" ) {
+       const botPerms = ['MANAGE_MESSAGES', 'KICK_MEMBERS', 'MANAGE_ROLES', 'MANAGE_CHANNELS'];
+
+	if (!message.guild.me.permissions.has(botPerms)) {
+		return message.reply(`I need the permissions ${botPerms.join(', ')} for this demonstration to work properly`);
+	}
+	if (message.guild.roles.cache.some(role => role.name === 'Mod')) {
+			return message.channel.send('A role with the name "Mod" already exists on this server.');
+		}
+
+		message.guild.roles.create({ data: { name: 'Mod', permissions: ['MANAGE_MESSAGES', 'KICK_MEMBERS'] } })
+			.then(() => message.channel.send('Created Mod role.'))
+			.catch(console.error);
+	}
+    if(command === "checkmod" ) {
+       const botPerms = ['MANAGE_MESSAGES', 'KICK_MEMBERS', 'MANAGE_ROLES', 'MANAGE_CHANNELS'];
+
+	if (!message.guild.me.permissions.has(botPerms)) {
+		return message.reply(`I need the permissions ${botPerms.join(', ')} for this demonstration to work properly`);
+	}
+		if (message.member.roles.cache.some(role => role.name === 'Mod')) {
+			return message.channel.send('You do have a role called Mod.');
+		}
+
+		message.channel.send('You don\'t have a role called Mod.');
+	}
+    if(command === "cankick" ) {
+       const botPerms = ['MANAGE_MESSAGES', 'KICK_MEMBERS', 'MANAGE_ROLES', 'MANAGE_CHANNELS'];
+
+	if (!message.guild.me.permissions.has(botPerms)) {
+		return message.reply(`I need the permissions ${botPerms.join(', ')} for this demonstration to work properly`);
+	}
+        if (message.member.hasPermission('KICK_MEMBERS')) {
+			return message.channel.send('You can kick members.');
+		}
+
+		message.channel.send('You cannot kick members.');
+	}
+    if (command === "makeprivate" ) {
+const botPerms = ['MANAGE_MESSAGES', 'KICK_MEMBERS', 'MANAGE_ROLES', 'MANAGE_CHANNELS'];
+
+	if (!message.guild.me.permissions.has(botPerms)) {
+		return message.reply(`I need the permissions ${botPerms.join(', ')} for this demonstration to work properly`);
+	}
+if (!message.channel.permissionsFor(client.user).has('MANAGE_ROLES')) {
+			return message.channel.send('Please make sure I have the `MANAGE_ROLES` permissions in this channel and retry.');
+		}
+
+		message.channel.overwritePermissions([
+			{
+				id: message.guild.id,
+				deny: ['VIEW_CHANNEL'],
+			},
+			{
+				id: client.user.id,
+				allow: ['VIEW_CHANNEL'],
+			},
+			{
+				id: message.author.id,
+				allow: ['VIEW_CHANNEL'],
+			},
+		])
+			.then(() => message.channel.send(`Made channel \`${message.channel.name}\` private.`))
+			.catch(console.error);
+	}
+    if (command === "createprivate" ) {
+const botPerms = ['MANAGE_MESSAGES', 'KICK_MEMBERS', 'MANAGE_ROLES', 'MANAGE_CHANNELS'];
+
+	if (!message.guild.me.permissions.has(botPerms)) {
+		return message.reply(`I need the permissions ${botPerms.join(', ')} for this demonstration to work properly`);
+	}
+message.guild.channels.create('private', {
+			type: 'text', permissionOverwrites: [
+				{
+					id: message.guild.id,
+					deny: ['VIEW_CHANNEL'],
+				},
+				{
+					id: message.author.id,
+					allow: ['VIEW_CHANNEL'],
+				},
+				{
+					id: client.user.id,
+					allow: ['VIEW_CHANNEL'],
+				},
+			],
+		})
+			.then(() => message.channel.send('Created a private channel.'))
+			.catch(console.error);
+	}
+    if (command === "unprivate" ) {
+const botPerms = ['MANAGE_MESSAGES', 'KICK_MEMBERS', 'MANAGE_ROLES', 'MANAGE_CHANNELS'];
+
+	if (!message.guild.me.permissions.has(botPerms)) {
+		return message.reply(`I need the permissions ${botPerms.join(', ')} for this demonstration to work properly`);
+	}
+if (!message.channel.permissionsFor(client.user).has('MANAGE_ROLES')) {
+			return message.channel.send('Please make sure i have the permissions MANAGE_ROLES in this channel and retry.');
+		}
+
+		message.channel.permissionOverwrites.get(message.guild.id).delete()
+			.then(() => message.channel.send(`Made channel ${message.channel.name} public.`))
+			.catch(console.error);
+	}
+    if (command === "myperms" ) {
+const botPerms = ['MANAGE_MESSAGES', 'KICK_MEMBERS', 'MANAGE_ROLES', 'MANAGE_CHANNELS'];
+
+	if (!message.guild.me.permissions.has(botPerms)) {
+		return message.reply(`I need the permissions ${botPerms.join(', ')} for this demonstration to work properly`);
+	}
+const finalPermissions = message.channel.permissionsFor(message.member);
+
+		message.channel.send(util.inspect(finalPermissions.serialize()), { code: 'js' });
+	}
+    if (command === "lockperms" ) {
+const botPerms = ['MANAGE_MESSAGES', 'KICK_MEMBERS', 'MANAGE_ROLES', 'MANAGE_CHANNELS'];
+
+	if (!message.guild.me.permissions.has(botPerms)) {
+		return message.reply(`I need the permissions ${botPerms.join(', ')} for this demonstration to work properly`);
+	}
+if (!message.channel.parent) {
+			return message.channel.send('This channel is not placed under a category.');
+		}
+
+		if (!message.channel.permissionsFor(client.user).has('MANAGE_ROLES')) {
+			return message.channel.send('Please make sure i have the permissions MANAGE_ROLES in this channel and retry.');
+		}
+
+		message.channel.lockPermissions()
+			.then(() => {
+				message.channel.send(`Synchronized overwrites of \`${message.channel.name}\` with \`${message.channel.parent.name}\`.`);
+			})
+			.catch(console.error);
+	}
+    if (command === "roleperms" ) {
+const botPerms = ['MANAGE_MESSAGES', 'KICK_MEMBERS', 'MANAGE_ROLES', 'MANAGE_CHANNELS'];
+
+	if (!message.guild.me.permissions.has(botPerms)) {
+		return message.reply(`I need the permissions ${botPerms.join(', ')} for this demonstration to work properly`);
+	}
+const roleFinalPermissions = message.channel.permissionsFor(message.member.roles.highest);
+
+		message.channel.send(util.inspect(roleFinalPermissions.serialize()), { code: 'js' });
+	}
     if(command === "kick") {
     if (!message.guild) return;
     // Assuming we mention someone in the message, this will return the user
