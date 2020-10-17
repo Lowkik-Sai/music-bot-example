d5ad3e2a852fbd34192bb84a939c91bd68e90164
Lowkik-Sai <64127668+Lowkik-Sai@users.noreply.github.com>
2020-10-17 15:13:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const ms = require("ms");
 const fs = require("fs");
 const Canvas = require('canvas');
 var jimp = require('jimp');
+var guesses;
+var num = 0;
 const Discord = require("discord.js");
 require("dotenv").config();
 require("./server.js");
@@ -27,6 +29,41 @@ bot.user.setPresence({ activity: { name: sts }, status: 'online' })
 .catch(console.error);
 },10000);
 
+bot.on('message', (message) => {
+    var mes = message.content.split(" ");
+    if(message.content == '+pick') {
+        message.reply('Picking a random number between 1 and 100');
+        num = Math.floor((Math.random() * 100) + 1);
+        guesses = 0;
+    }
+    if(mes[0] == '+guess') {
+        if (num == 0)
+        {
+            message.reply('Picking a random number between 1 and 100');
+            num = Math.floor((Math.random() * 100) + 1);
+            guesses = 0;
+        }
+        else if(mes[1] == num)
+        {
+            guesses++;
+            message.reply('You got it! Only took ' + guesses + ' tries.');
+            message.reply('Picking a random number between 1 and 100');
+            num = Math.floor((Math.random() * 100) + 1);
+            guesses = 0;
+        }
+        else if(mes[1] < num)
+        {
+            message.reply(mes[1] + ' is too low');
+            guesses++;
+        }
+        else if(mes[1] > num)
+        {
+            message.reply(mes[1] + ' is too high');
+            guesses++;
+        }
+    }
+
+});
 
 bot.on('guildCreate', async guild => {
 	const fetchedLogs = await guild.fetchAuditLogs({
