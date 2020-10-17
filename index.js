2c7c5dcc4c9a173664bea23c242785c4630b006c
Lowkik-Sai <64127668+Lowkik-Sai@users.noreply.github.com>
2020-10-17 16:16:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -45,20 +45,20 @@ bot.on('message', message => {
         }, 1000);
     }
     if(message.content == "+viewnumb") {
-        if(!message.author.id == ownerID) return message.reply(`You don't have the permission to run this command.`);
+        if(message.author.id !== ownerID) return message.reply(`You don't have the permission to run this command.`);
         message.reply(`The current number is ${number}`);
     }
     if(message.content == "+viewlim") {
-        if(!message.author.id == ownerID) return message.reply(`You don't have the permission to run this command.`);
+        if(message.author.id !== ownerID) return message.reply(`You don't have the permission to run this command.`);
         message.reply(`The current limit is ${limit}`);
     }
     if(message.content == "+reroll") {
-        if(!message.author.id == ownerID) return message.reply(`You don't have the permission to run this command.`);
+        if(message.author.id !== ownerID) return message.reply(`You don't have the permission to run this command.`);
         number = Math.floor(Math.random()* Math.floor(limit));
         message.author.send(`The new number is ${number}`);
     }
     if(message.content.startsWith("+number")) {
-        if(!message.author.id == ownerID) return message.reply(`You don't have the permission to run this command.`);
+        if(message.author.id !== ownerID) return message.reply(`You don't have the permission to run this command.`);
         const args = message.content.slice(1).trim().split(/ +/g);
         const newNumb = args.slice(1).join(" ");
         if(!newNumb) return message.reply(`You didn't specified a new number.`);
@@ -66,7 +66,7 @@ bot.on('message', message => {
         message.reply(`The number has been successfully changed to ${newNumb}!`);
     }
 	if(message.content.startsWith("+limit")) {
-        if(!message.author.id == ownerID) return message.reply(`You don't have the permission to run this command.`);
+        if(message.author.id !== ownerID) return message.reply(`You don't have the permission to run this command.`);
         const args = message.content.slice(1).trim().split(/ +/g);
         const newLim = args.slice(1).join(" ");
         if(!newLim) return message.reply(`You didn't specified a new limit.`);
@@ -79,7 +79,7 @@ bot.on('message', message => {
             if(message.content > limit) return message.reply(`The number is between 1 and ${limit}! Try again`).then(sent => sent.delete(10000));
             if(message.content < 1) return message.reply(`The number cannot be negative! Try again`).then(sent => sent.delete(10000));
             if(message.content == number) {
-                var everyone =  message.guild.roles.cache.find("name", "@everyone");
+                var everyone =  message.guild.roles.cache.find(e => e.name === '@everyone');
                 bot.channels.find("id", channelID).overwritePermissions(everyone, {
                     VIEW_CHANNEL: true,
                     SEND_MESSAGES: false
