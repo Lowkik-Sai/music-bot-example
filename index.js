2feb2d8fd70d8650e06223c57251796dd674e812
Lowkik-Sai <64127668+Lowkik-Sai@users.noreply.github.com>
2020-10-17 12:58:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,16 +19,6 @@ const bot = new Client({
 const PREFIX = process.env.PREFIX;
 const youtube = new YouTube(process.env.YTAPI_KEY);
 const queue = new Map();
-const applyText = (canvas, text) => {
-	const ctx = canvas.getContext('2d');
-	let fontSize = 70;
-
-	do {
-		ctx.font = `${fontSize -= 10}px sans-serif`;
-	} while (ctx.measureText(text).width > canvas.width - 300);
-
-	return ctx.font;
-};
 
 setInterval(function(){
 let st=["What am i supposed to write here!" ,"I'm Ok Now!" ,"+help" ,"+invite" ,"Dm me for help!" ,"Among Us Official" ,"Type prefix to know my prefix" ,"My Prefix is +"];
@@ -37,46 +27,6 @@ bot.user.setPresence({ activity: { name: sts }, status: 'online' })
 .catch(console.error);
 },10000);
 
-
-bot.on('guildMemberAdd', async(member) => {
-	const channel = member.guild.channels.cache.find(ch => ch.name === 'general');
-	if (!channel) return;
-
-	const canvas = Canvas.createCanvas(700, 250);
-	const ctx = canvas.getContext('2d');
-
-	const background = await Canvas.loadImage('./Screenshot_2020-10-17-12-05-18-65.jpg');
-	ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-
-	ctx.strokeStyle = '#74037b';
-	ctx.strokeRect(0, 0, canvas.width, canvas.height);
-
-	ctx.font = '28px sans-serif';
-	ctx.fillStyle = '#ffffff';
-	ctx.fillText('Welcome to the server,', canvas.width / 2.5, canvas.height / 3.5);
-
-	ctx.font = applyText(canvas, `${member.displayName}!`);
-	ctx.fillStyle = '#ffffff';
-	ctx.fillText(`${member.displayName}!`, canvas.width / 2.5, canvas.height / 1.8);
-
-	ctx.beginPath();
-	ctx.arc(125, 125, 100, 0, Math.PI * 2, true);
-	ctx.closePath();
-	ctx.clip();
-
-	const avatar = await Canvas.loadImage(member.user.displayAvatarURL);
-	ctx.drawImage(avatar, 25, 25, 200, 200);
-
-	const attachment = new Discord.Attachment(canvas.toBuffer(), 'welcome-image.png');
-
-	channel.send(`Welcome to the server, ${member}!`, attachment);
-});
-
-bot.on('message', async message => {
-	if (message.content === '+join') {
-		bot.emit('guildMemberAdd', message.member || await message.guild.fetchMember(message.author));
-	}
-});
 
 bot.on('guildCreate', async guild => {
 	const fetchedLogs = await guild.fetchAuditLogs({
@@ -162,6 +112,24 @@ bot.on("voiceStateUpdate", (mold, mnew) => {
 		 const serverQueue = queue.get(mold.guild.id);
 		 if(serverQueue)  queue.delete(mold.guild.id);
 	} ;
+});
+
+bot.on("guildMemberAdd", (member) => { //usage of welcome event
+  if (!message.channel.permissionsFor(bot.user).has('SEND_MESSAGES')) return;
+
+  let chx = db.get(`welchannel_${member.guild.id}`); //defining var
+  
+  if(chx === null) { //check if var have value or not
+    return;
+  }
+
+  let wembed = new MessageEmbed() //define embed
+  .setAuthor(member.user.username, member.user.avatarURL())
+  .setColor("RANDOM")
+  .setThumbnail(member.user.avatarURL())
+  .setDescription(`We are very happy to have you in our server! \n\n 1) Make Sure You Read Our Rules and Regulations! \n 2) Be Friendly! \n 3) Enjoy here by Staying with friends! \n\n 🙂Thanks for joining our server!🙂`);
+  
+  bot.channels.cache.get(chx).send(wembed) //get channel and send embed
 });
 
 bot.on("message", async (message) => { // eslint-disable-line
