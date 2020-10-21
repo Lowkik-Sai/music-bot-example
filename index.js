a0712dd4aa51d8c39e5f27b4f17866761927da17
Lowkik-Sai <64127668+Lowkik-Sai@users.noreply.github.com>
2020-10-21 16:01:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1440,6 +1440,36 @@ const ms = require("parse-ms");
     message.channel.send(moneyEmbed)
 
 }
+    if (command === "gamble" ) {
+let percents = ["3,51", "3,91", "4,00", "4,31", "4,72", "4,99"]
+    let math = Math.floor(Math.random() * percents.length)
+    
+    let money = await db.fetch(`money_${message.guild.id}_${user.id}`);
+
+    let amout = args[0];
+    if (!amout) return message.channel.send("You have to specify the coins!");
+    if (money < amout)
+      return message.channel.send("You do not have enough Coins!");
+    if (amout.includes("-"))
+      return message.channel.send("Looks like your try to Gamble with Minus Numbers, that won't work");
+    let result = ["win", "lose"];
+    let resault = Math.floor(Math.random() * result.length);
+    if (result[resault] === "lose") {
+      db.subtract(`money_${message.guild.id}_${user.id}`, amout);
+      message.channel.send(`You lose!`);
+    }
+    if (result[resault] === "win") {
+      db.add(`money_${message.guild.id}_${user.id}`, 3 * amout);
+
+      let embed = new Discord.RichEmbed()
+      .setAuthor("Gambling Machine")
+      .setDescription(`You won: **${amout}** coins\nYour Bet got multiplied by **3**\n\nYou now have ${money} coins`)
+      .setColor("#32cd32")
+      
+      message.channel.send(embed);
+      return;
+    }
+  }
     if (command === "bet" ) {
 	if (message.guild === null){
             message.reply(DMMessage);
