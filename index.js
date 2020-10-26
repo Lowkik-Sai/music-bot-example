38e1039e3fbe69cef64dfdab37162f44c58c836b
Lowkik-Sai <64127668+Lowkik-Sai@users.noreply.github.com>
2020-10-26 15:44:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2303,45 +2303,6 @@ const embed = new MessageEmbed()
     message.channel.send(embed).then(msg => {msg.delete(10000)});
 
     }
-    if (command === "eval" ) {
-        
-        let Owner = message.author;
-    if(Owner.id !== "654669770549100575" && Owner.id !== "213588167406649346") return message.reply({embed: {
-    color: 3066993,
-    description:"Only the bot owner can use this command!"
-}});
-   const embed = new MessageEmbed()
-            .setTitle('Evaluating...')
-        const msg = await message.channel.send(embed);
-        try {
-            const data = eval(args.join(' ').replace(/```/g, ''));
-            const embed = new MessageEmbed()
-                .setTitle('Output: ')
-                .setDescription(await data)
-            await msg.edit(embed)
-            await msg.react('✅')
-            await msg.react('❌')
-            const filter = (reaction, user) => (reaction.emoji.name === '❌' || reaction.emoji.name === '✅') && (user.id === message.author.id);
-            msg.awaitReactions(filter, { max: 1 })
-                .then((collected) => {
-                    collected.map((emoji) => {
-                        switch (emoji._emoji.name) {
-                            case '✅':
-                                msg.reactions.removeAll();
-                                break;
-                            case '❌':
-                                msg.delete()
-                                break;
-                        }
-                    })
-                })
-        } catch (e) {
-            const embed = new MessageEmbed()
-                .setTitle('An Error has occured')
-            return await msg.edit(embed);
-
-        }
-    }
     if (command === "morse" ) {
       
     let alpha = " ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890".split(""),
