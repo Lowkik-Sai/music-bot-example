1d318602b501a2a329be91c4b6d76ed56193795e
Lowkik-Sai <64127668+Lowkik-Sai@users.noreply.github.com>
2020-10-26 11:02:53 +0530

    Create index.js

--- index.js
+++ index.js
@@ -3810,6 +3810,24 @@ const roleFinalPermissions = message.channel.permissionsFor(message.member.roles
       message.reply("You didn't mention the user to kick!");
     }
   }
+    if (command === "unban" ){
+const member = args[0];
+
+        if (!member) {
+             return message.channel.send(`Please enter a id!`)
+        }
+
+        try {
+            message.guild.fetchBans().then(bans => {
+                message.guild.members.unban(member)
+            })
+            await message.channel.send(`${member} has been unbanned!`)
+        } catch (e) {
+            return message.channel.send(`An error occured!`)
+        }
+
+    }
+}
     if(command === "ban") {
     if (!message.guild) return;
     // Assuming we mention someone in the message, this will return the user
