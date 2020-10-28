20fccce81396569287cad424eaf799dd12f040c8
Lowkik-Sai <64127668+Lowkik-Sai@users.noreply.github.com>
2020-10-28 08:42:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1919,7 +1919,7 @@ let user = message.author;
        
 
        let embed = new MessageEmbed()
-    .setDescription("**VIP Ranks**\n\nBronze: 3500 Coins [m!buy bronze]\n\n**Lifestyle Items**\n\nFresh Nikes: 600 [m!buy nikes]\nCar: 800 [m!buy car]\nMansion: 1200 [m!buy mansion]")
+    .setDescription("**VIP Ranks**\n\nBronze: 3500 Coins [+buy bronze]\n\n**Lifestyle Items**\n\nFresh Nikes: 600 [+buy nikes]\nCar: 800 [+buy car]\nMansion: 1200 [+buy mansion]")
     .setColor("RANDOM")
     message.channel.send(embed)
 
