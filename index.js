a3fc52f29c3a104ab0f006915cb7a35f073522a3
Lowkik-Sai <64127668+Lowkik-Sai@users.noreply.github.com>
2020-10-30 13:27:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -732,10 +732,7 @@ let questions = [
    description: `Currently,There is nearly ${questions.length} questions, if you have any suggestions/ideas for questions please use '${PREFIX}trivia suggest <question, 4 possible answers, and correct answer>'.`
 }})
         }else if(args[0] === 'suggest' || args[0] === '2'){
-          bot.users.cache.get('654669770549100575').send({embed: {
-   color: 3066993,
-   description: Suggested by : message.author.tag + `\n ${args.join(" ").slice(8)}`
-}})
+          bot.users.cache.get('654669770549100575').send(Suggested by : message.author.tag + `\n ${args.join(" ").slice(8)}`)
           console.log(message.content.length)
         }else if(args[0] === 'start' || args[0] === '3'){
           let q = questions[Math.floor(Math.random() * questions.length)];
