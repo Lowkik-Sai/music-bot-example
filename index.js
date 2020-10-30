1893ab8cc593171a0f91d426d745a0c2dd26e266
Lowkik-Sai <64127668+Lowkik-Sai@users.noreply.github.com>
2020-10-30 08:00:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3109,6 +3109,37 @@ const translate = require('google-translate-api');
             }
         }
 	}
+    if (command === "worldnews" ) {
+try {
+      const response = await fetch(
+        `https://newsapi.org/v2/top-headlines?sources=reuters&pageSize=5&apiKey=${newsAPI}`
+      );
+      const json = await response.json();
+      const articleArr = json.articles;
+      let processArticle = article => {
+        const embed = new MessageEmbed()
+          .setColor('#FF4F00')
+          .setTitle(article.title)
+          .setURL(article.url)
+          .setAuthor(article.author)
+          .setDescription(article.description)
+          .setThumbnail(article.urlToImage)
+          .setTimestamp(article.publishedAt)
+          .setFooter('powered by NewsAPI.org');
+        return embed;
+      };
+      async function processArray(array) {
+        for (const article of array) {
+          const msg = await processArticle(article);
+          message.channel.send(msg);
+        }
+      }
+      await processArray(articleArr);
+    } catch (e) {
+      message.channel.send('Something failed along the way');
+      return console.error(e);
+    }
+  }
     if (command === "usnews" ) {
 try {
       const response = await fetch(
@@ -3143,7 +3174,7 @@ try {
     if (command === "indnews" ) {
 try {
       const response = await fetch(
-        `https://newsapi.org/v2/top-headlines?country=india&pageSize=5&apiKey=${newsAPI}`
+        `https://newsapi.org/v2/top-headlines?country=ind&pageSize=5&apiKey=${newsAPI}`
       );
       const json = await response.json();
       const articleArr = json.articles;
