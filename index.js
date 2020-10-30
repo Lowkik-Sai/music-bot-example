b2bc92ea884f8c73efdb2cd98062e2113f649a62
Lowkik-Sai <64127668+Lowkik-Sai@users.noreply.github.com>
2020-10-30 07:50:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3140,6 +3140,37 @@ try {
       return console.error(e);
     }
   }
+    if (command === "indnews" ) {
+try {
+      const response = await fetch(
+        `https://newsapi.org/v2/top-headlines?country=india&pageSize=5&apiKey=${newsAPI}`
+      );
+      const json = await response.json();
+      const articleArr = json.articles;
+      let processArticle = article => {
+        const embed = new MessageEmbed()
+          .setColor('#FF4F00')
+          .setTitle(article.title)
+          .setURL(article.url)
+          .setAuthor(article.author)
+          .setDescription(article.description)
+          .setThumbnail(article.urlToImage)
+          .setTimestamp(article.publishedAt)
+          .setFooter('powered by NewsAPI.org');
+        return embed;
+      };
+      async function processArray(array) {
+        for (const article of array) {
+          const msg = await processArticle(article);
+          message.channel.send(msg);
+        }
+      }
+      await processArray(articleArr);
+    } catch (e) {
+      message.channel.send('Something failed along the way');
+      return console.error(e);
+    }
+  }
     if (command === "covid" ) { 
        
 
