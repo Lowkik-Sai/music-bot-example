25ca4a0ba36393f99fb2c3f399e8c3da2e81574d
Lowkik-Sai <64127668+Lowkik-Sai@users.noreply.github.com>
2020-10-31 19:04:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1597,6 +1597,28 @@ let money = await db.fetch(`money_${message.guild.id}_${user.id}`);
 }});
   }, ms(mutetime));
 }
+    if (command === "pol" ) {
+if (!message.member.hasPermission('MANAGE_GUILD') && message.author.id !== '357555941215961099') return message.channels.send('Sorry, you don/t have permission to create poll!').then(msg => msg.delete({timeout: 10000}));
+  if (!args.join(' ')) return message.channel.send('Usage: poll <title>').then(msg => msg.delete({timeout: 10000}));
+  
+  const embed = new Discord.RichEmbed()
+    .setTitle(args.join(' '))
+    .setFooter('DROP A VOTE!')
+    .setColor('#7289DA')
+    const pollTitle = await message.channel.send({ embed });
+      await pollTitle.react(`👍`);
+      await pollTitle.react(`👎`);
+      
+    const filter = (reaction) => reaction.emoji.name === '👍';
+    const collector = pollTitle.createReactionCollector(filter, { time: 15000 });
+      collector.on('collect', r => console.log(`Collected ${r.emoji.name}`));
+      collector.on('end', collected => console.log(`Collected ${collected.size} items`));
+  
+    const filter1 = (reaction) => reaction.emoji.name === '👎';
+    const collector1 = pollTitle.createReactionCollector(filter1, { time: 15000 });
+      collector1.on('collect', r => console.log(`Collected ${r.emoji.name}`));
+      collector1.on('end', collected => console.log(`Collected ${collected.size} items`));
+}
     if (command === "poll" ) {
     if (!message.member.permissions.has("ADMINISTRATOR"))
       return message.channel.send({embed: {
