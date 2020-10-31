24d4f64dabdfe8fa3f96bae752e370834ffbf4d2
Lowkik-Sai <64127668+Lowkik-Sai@users.noreply.github.com>
2020-10-31 08:53:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -26,6 +26,11 @@ const bot = new Client({
     disableMentions: "everyone"
 });
 
+const serverStats = {
+    guildID: '763233532369567765',
+    ticketCategoryID: '771936611063038012'
+}
+
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
 }
@@ -71,7 +76,7 @@ let abyssTwothreethree = new cron.CronJob('0 */10 * * * *', test => {
 let scheduledXdress = new cron.CronJob('* * * * *', test => {
   // This runs every Tue and Sun at 20:00:00
   // 2000 local is 1200 
-  let abyss_channel = client.channels.cache.get(`675356163432513536`)
+  let abyss_channel = bot.channels.cache.get(`675356163432513536`)
   abyss_channel.send("crossdress <@" + '471300810194681866' + ">")
   // pings hibiki
 });
@@ -1229,14 +1234,14 @@ class Drawing {
         this.drawCanvas(true);
         this.message.edit(`**Thanks for drawing with us!**` + reason);
         this.message.clearReactions();
-        this.message.client.clearTimeout(this.timeout);
+        this.message.bot.clearTimeout(this.timeout);
         channels = channels.filter(item => item !== this.message.channel.id);
     }
 
     renewTimeout() {
         let self = this;
-        this.message.client.clearTimeout(this.timeout);
-        this.timeout = this.message.client.setTimeout(function() {
+        this.message.bot.clearTimeout(this.timeout);
+        this.timeout = this.message.bot.setTimeout(function() {
             self.stop("**\nEnd Reason: Timeout (2 minutes)**");
         }, 120000);
     }
@@ -5058,6 +5063,143 @@ function play(guild, song) {
         }
     });
 }
+
+bot.on('message', async message => {
+    if (message.author.bot) return;
+    if (message.channel.type !== 'text') {
+        let active = await db.fetch(`support_${message.author.id}`);
+        let guild = bot.guilds.get(serverStats.guildID);
+        let channel, found = true;
+        try {
+            if (active) bot.channels.get(active.channelID)
+                .guild;
+        } catch (e) {
+            found = false;
+        }
+        if (!active || !found) {
+            active = {};
+            channel = await guild.createChannel(`${message.author.username}-${message.author.discriminator}`)
+            channel.setParent(serverStats.ticketCategoryID)
+            channel.setTopic(`-close to close the ticket | Support for ${message.author.tag} | ID: ${message.author.id}`)
+
+            channel.overwritePermissions("547412713152970784", {   ///Private crack
+                VIEW_CHANNEL: false,
+                SEND_MESSAGES: false,
+                ADD_REACTIONS: false
+            })
+            channel.overwritePermissions("540746015528058899", {   ///all time supporter
+                VIEW_CHANNEL: false,
+                SEND_MESSAGES: false,
+                ADD_REACTIONS: false
+            })
+            channel.overwritePermissions("541666658356297729", {   ///coin bot access
+                VIEW_CHANNEL: false,
+                SEND_MESSAGES: false,
+                ADD_REACTIONS: false
+            })
+            channel.overwritePermissions("544893073856004097", {   ///paid member
+                VIEW_CHANNEL: false,
+                SEND_MESSAGES: false,
+                ADD_REACTIONS: false
+            })
+            channel.overwritePermissions("532629827732373534", {   ///bot fixer
+                VIEW_CHANNEL: false,
+                SEND_MESSAGES: false,
+                ADD_REACTIONS: false
+            })
+            channel.overwritePermissions("544588537606176768", {   ///bot controler
+                VIEW_CHANNEL: false,
+                SEND_MESSAGES: false,
+                ADD_REACTIONS: false
+            })
+            channel.overwritePermissions("547649177669206016", {   ///trivia crack member
+                VIEW_CHANNEL: false,
+                SEND_MESSAGES: false,
+                ADD_REACTIONS: false
+            })
+            channel.overwritePermissions("544225979103576065", {   ///moderator
+                VIEW_CHANNEL: false,
+                SEND_MESSAGES: false,
+                ADD_REACTIONS: false
+            })
+            channel.overwritePermissions("543840213651161101", {   ///tickety
+                VIEW_CHANNEL: false,
+                SEND_MESSAGES: false,
+                ADD_REACTIONS: false
+            });
+
+
+
+            let author = message.author;
+            const newChannel = new Discord.RichEmbed()
+                .setColor('RANDOM')
+                .setAuthor(author.tag, author.avatarURL)
+                .setFooter('Support Ticket Created!')
+                .addField('User', author)
+                .addField('ID', author.id)
+            await channel.send(newChannel);
+            const newTicket = new Discord.RichEmbed()
+                .setColor('RANDOM')
+                .setAuthor(`Hello, ${author.username}`, author.avatarURL)
+                .setFooter('Support Ticket Created!')
+            await author.send(newTicket);
+            active.channelID = channel.id;
+            active.targetID = author.id;
+        }
+        channel = bot.channels.get(active.channelID);
+        const dm = new Discord.RichEmbed()
+            .setColor('RANDOM')
+            .setAuthor(`Thank you, ${message.author.username}`, message.author.avatarURL)
+            .setFooter(`Your message has been sent - A staff member will be in contact soon.`)
+        await message.author.send(dm);
+        if (message.content.startsWith('?complete')) return;
+        const embed5 = new Discord.RichEmbed()
+            .setColor('RANDOM')
+            .setAuthor(message.author.tag, message.author.avatarURL)
+            .setDescription(message.content)
+            .setFooter(`Message Received - ${message.author.tag}`)
+        await channel.send(embed5);
+        db.set(`support_${message.author.id}`, active);
+        db.set(`supportChannel_${channel.id}`, message.author.id);
+        return;
+    }
+    let support = await db.fetch(`supportChannel_${message.channel.id}`);
+    if (support) {
+        support = await db.fetch(`support_${support}`);
+        let supportUser = bot.users.get(support.targetID);
+        if (!supportUser) return message.channel.delete();
+        if (message.content.toLowerCase() === '.close') {
+            const complete = new Discord.RichEmbed()
+                .setColor('RANDOM')
+                .setAuthor(`Hey, ${supportUser.tag}`, supportUser.avatarURL)
+                .setFooter('Ticket Closed -- Team TRIVIA CRACK')
+                .setDescription('*Your ticket has been marked as complete. If you wish to reopen it, or create a new one, please send a message to the bot.*')
+            supportUser.send(complete);
+            message.channel.delete();
+            db.delete(`support_${support.targetID}`);
+            let inEmbed = new Discord.RichEmbed()
+                .setTitle('Ticket Closed!')
+                .addField('Support User', `${supportUser.tag}`)
+                .addField('Closer', message.author.tag)
+                .setColor('RANDOM')
+            const staffChannel = bot.channels.get('545474922466639892'); //Create a log channel and put id here
+            staffChannel.send(inEmbed);
+        }
+        const embed4 = new Discord.RichEmbed()
+            .setColor('RANDOM')
+            .setAuthor(message.author.tag, message.author.avatarURL)
+            .setFooter(`Message Received - TRIVIA CRACK`)
+            .setDescription(message.content)
+        bot.users.get(support.targetID)
+            .send(embed4);
+        message.delete({
+            timeout: 10000
+        });
+        embed4.setFooter(`Message Sent -- ${supportUser.tag}`)
+            .setDescription(message.content);
+        return message.channel.send(embed4);
+    }
+});
 
 bot.login(process.env.BOT_TOKEN);
 
