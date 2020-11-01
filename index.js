0d362f07807241cc7797c53351c1e932c94aa963
Lowkik-Sai <64127668+Lowkik-Sai@users.noreply.github.com>
2020-11-01 19:21:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -526,6 +526,36 @@ bot.on("message", async (message) => { // eslint-disable-line
         const guild = bot.guilds.cache.get(guildId);
         await guild.channels.cache.find(channel => channel.name === 'applications').send(embed);
     }
+    if (command === "seizure") {
+    const emoji1 = '🇳'
+    const emoji = '🇾'
+    message.channel.send('Attention: This command could give you a mini seizure.. Do you want to continue?\nBy accepting you are responsible for giving other people seizures.').then(msg => {
+        msg.react(emoji).then(r => {
+            msg.react(emoji1)
+            const yes = (reaction, user) => reaction.emoji.name === emoji && user.id === message.author.id;
+            const nopleas = (reaction, user) => reaction.emoji.name === emoji1 && user.id === message.author.id;
+            const sure = msg.createReactionCollector(yes, {
+                time: 1000000
+            });
+            const no = msg.createReactionCollector(nopleas, {
+                time: 1000000
+            });
+            sure.on('collect', r => {
+                msg.delete();
+                const emb = new A.RichEmbed()
+                    .setColor(0xFFFF00)
+                    .setImage('https://cdn.glitch.com/ce500e3d-b500-47a8-a6a8-c0b5657d808c%2FWebp.net-gifmaker.gif')
+                    .setFooter(`Requested by: ${message.author.tag}`);
+                message.channel.send({
+                    embed: emb
+                })
+            })
+            no.on('collect', r => {
+                msg.delete();
+            })
+        })
+    })
+}
     if (command === "pages" ) {
     
 let pages = ['Page one!', 'Second page', 'Third page']
