da1497cb9ccf9d5ef74f7c05af01f50a7829f29a
Lowkik-Sai <64127668+Lowkik-Sai@users.noreply.github.com>
2020-11-01 08:16:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -667,7 +667,7 @@ const options = {
                                  .setFooter(`Question: ${item.q}`)
                                  .setColor(`${message.guild.me.displayHexColor!=='#000000' ? message.guild.me.displayHexColor : 0xffffff}`)
                                 })
-  } catch (_) {
+  } catch (e) {
     return message.channel.send({embed: new MessageEmbed()
                                  .setAuthor('No one got the answer in time!')
                                  .setTitle(`Correct Answer(s): \`${item.a}\``)
