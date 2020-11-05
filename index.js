2b02b1e118663e6d94ed80bcb5c4adec7e7fe9d4
Lowkik-Sai <64127668+Lowkik-Sai@users.noreply.github.com>
2020-11-05 07:55:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -959,19 +959,19 @@ const reason = message.content.split(" ").slice(1).join(" ");
         c.overwritePermissions([
   {
      id: role.id,
-     allow: ['SEND_MESSAGES', 'READ_MESSAGES'],
+     allow: ['SEND_MESSAGES'], ['READ_MESSAGES'],
   },
 ]);
         c.overwritePermissions([
   {
      id: message.guild.id,
-     deny: ['SEND_MESSAGES', 'READ_MESSAGES'],
+     deny: ['SEND_MESSAGES'], ['READ_MESSAGES'],
   },
 ]);
         c.overwritePermissions([
   {
      id: message.author.id,
-     allow: ['SEND_MESSAGES', 'READ_MESSAGES'],
+     allow: ['SEND_MESSAGES'], ['READ_MESSAGES'],
   },
 ]);
         const embed2 = new MessageEmbed()
