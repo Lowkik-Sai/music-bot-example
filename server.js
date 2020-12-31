b387f084773164a3be0ff71fb326405676b74f26
Lowkik-Sai <64127668+Lowkik-Sai@users.noreply.github.com>
2020-12-31 21:19:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,12 @@ var express = require("express");
 var http = require("http");
 var app = express();
 
+const wakeDyno = require("woke-dyno");
 
+// start the server, then call wokeDyno(url).start()
+app.listen(process.env.PORT, () => {
+    wakeDyno(`https://among-us-bot-india.herokuapp.com/`).start(); // DYNO_URL should be the url of your Heroku app
+});
 // Ping the app
 app.use(express.static("public"));
 // http://expressjs.com/en/starter/basic-routing.html
