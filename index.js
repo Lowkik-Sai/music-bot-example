ed923b06289cb8d12add4afa834bd4805bad76db
Lowkik-Sai <64127668+Lowkik-Sai@users.noreply.github.com>
2021-01-02 14:00:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -922,25 +922,7 @@ bot.on("ready", async () => {
 });
 
 bot.on("message", async message => {
-  db.add(`messages_${message.guild.id}_${message.author.id}`, 1)
-  let messagefetch = db.fetch(`messages_${message.guild.id}_${message.author.id}`)
-
-  let messages;
-  if (messagefetch == 25) messages = 25; //Level 1
-  else if (messagefetch == 65) messages = 65; // Level 2
-  else if (messagefetch == 115) messages = 115; // Level 3
-  else if (messagefetch == 200) messages = 200; // Level 4
-  else if (messagefetch == 300) messages = 300; // Level 5
-
-  if (!isNaN(messages)) {
-    db.add(`level_${message.guild.id}_${message.author.id}`, 1)
-    let levelfetch = db.fetch(`level_${message.guild.id}_${message.author.id}`)
-
-    let levelembed = new Discord.MessageEmbed()
-      .setDescription(`${message.author}, You have leveled up to level ${levelfetch}`)
-    message.channel.send(levelembed)
-  }
-
+  
   if (message.channel.id === giveawayChannel) {
     if (giveawayActive && !message.author.bot && !blacklist.includes(message.author.id)) {
         if (!roleblacklist.some(role => { if(message.member.roles.cache.has(role)) return true; })) {
